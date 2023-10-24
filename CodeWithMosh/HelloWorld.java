2a5363311ea5dae10bf609c062e129002f886b24
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-10-25 00:40:01 +0530

    Hello World in java

---
+++ CodeWithMosh/HelloWorld.java
@@ -0,0 +1,8 @@
+package CodeWithMosh;
+
+public class HelloWorld {
+    // Hello World program in Java.
+    public static void main(String... args) {
+        System.out.println("HelloWorld");
+    }
+}
