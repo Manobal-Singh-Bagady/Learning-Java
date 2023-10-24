38eded0ba6375109db4da8460abe543770eb537b
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-10-25 00:40:49 +0530

    Learning Variables in java

---
+++ CodeWithMosh/Variables.java
@@ -0,0 +1,10 @@
+package CodeWithMosh;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Learning variables in java.
+        byte manobalAge = 19;
+        manobalAge++;
+        System.out.println(manobalAge);
+    }
+}
