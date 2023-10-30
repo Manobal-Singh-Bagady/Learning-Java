c15cbbbaf6bd6b43330819a9a5018257e8f29c9d
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-10-31 02:04:09 +0530

    delete HelloWorld.java and make Main.java into HelloWorld program.

--- CodeWithMosh/src/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    // Hello World program in Java.
-    public static void main(String... args) {
-        System.out.println("HelloWorld");
-    }
-}
--- CodeWithMosh/src/Main.java
+++ CodeWithMosh/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // Hello World program in Java.
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
