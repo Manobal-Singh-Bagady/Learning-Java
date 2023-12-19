bb9d6c1760595113341261f9bb1b514ed14e0d04
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-12-20 00:50:59 +0530

    ✅ 2. Anatomy of a Java Program.

---
+++ UltimateJavaCourse/Part1Fundamentals/_1GettingStarted/_2AnatomyOfAJavaProgram.java
@@ -0,0 +1,15 @@
+package UltimateJavaCourse.Part1Fundamentals._1GettingStarted;
+
+public class _2AnatomyOfAJavaProgram {
+    // public -> access modifier
+    // class -> keyword to make a class
+    // Main -> name of the class in PascalCase
+    public class Main {
+        // public -> access modifier
+        // void -> return type
+        // main -> name of the method in camelCase
+        public void main(){
+
+        }
+    }
+}
