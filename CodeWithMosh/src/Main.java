fb56d1f0fffb08e753ac46a375c381cfbebe147c
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-12-23 19:48:38 +0530

    Delete unused code files

--- CodeWithMosh/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    // Hello World program in Java.
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- CodeWithMosh/src/Variables.java
+++
@@ -1,8 +0,0 @@
-public class Variables {
-    public static void main(String[] args) {
-        // Learning variables in java.
-        byte manobalAge = 19;
-        manobalAge++;
-        System.out.println(manobalAge);
-    }
-}
