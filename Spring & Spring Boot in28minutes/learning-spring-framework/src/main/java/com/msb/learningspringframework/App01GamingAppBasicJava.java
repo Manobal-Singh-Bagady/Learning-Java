71f39f593ee346e42069870c6d7a081596420fc0
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2024-01-02 02:00:33 +0530

    Commented out SuperContraGame instantiation

--- Spring & Spring Boot in28minutes/learning-spring-framework/src/main/java/com/msb/learningspringframework/App01GamingAppBasicJava.java
+++ Spring & Spring Boot in28minutes/learning-spring-framework/src/main/java/com/msb/learningspringframework/App01GamingAppBasicJava.java
@@ -2,12 +2,12 @@ package com.msb.learningspringframework;
 
 import com.msb.learningspringframework.game.GameRunner;
 import com.msb.learningspringframework.game.MarioGame;
-import com.msb.learningspringframework.game.SuperContraGame;
+// import com.msb.learningspringframework.game.SuperContraGame;
 
 public class App01GamingAppBasicJava {
     public static void main(String[] args) {
         var game = new MarioGame();
-        var game1 = new SuperContraGame();
+        // var game1 = new SuperContraGame();
         var gameRunner = new GameRunner(game);
         gameRunner.runGame();
     }
